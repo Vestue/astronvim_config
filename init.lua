4eb1a7bbbcfb358d36f447673f6ac28359256e2a
Vestue <61783376+Vestue@users.noreply.github.com>
2022-10-15 16:08:48 +0200

    Fix transparent bg

--- init.lua
+++ init.lua
@@ -30,9 +30,20 @@ local config = {
 
     -- Add highlight groups in any theme
     highlights = {
-        -- init = { -- this table overrides highlights in all themes
-        --   Normal = { bg = "#000000" },
-        -- }
+        init = { -- this table overrides highlights in all themes
+            --Normal = { bg = "#000000" },
+            -- set the transparency for all of these highlight groups
+            Normal = { bg = "NONE", ctermbg = "NONE" },
+            NormalNC = { bg = "NONE", ctermbg = "NONE" },
+            CursorColumn = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
+            CursorLine = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
+            CursorLineNr = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
+            LineNr = {},
+            SignColumn = {},
+            StatusLine = {},
+            NeoTreeNormal = { bg = "NONE", ctermbg = "NONE" },
+            NeoTreeNormalNC = { bg = "NONE", ctermbg = "NONE" },
+        }
         -- duskfox = { -- a table of overrides/changes to the duskfox theme
         --   Normal = { bg = "#000000" },
         -- },
