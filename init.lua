85284ab6982766146ea0aa6f55023be5c256ad05
Vestue <61783376+Vestue@users.noreply.github.com>
2023-05-01 16:10:31 +0200

    Add initial setup for catppuccin

--- init.lua
+++ init.lua
@@ -19,6 +19,57 @@ return {
 
   -- Set colorscheme to use
   colorscheme = "astrodark",
+  --colorscheme = "catppuccin",
+
+  plugins = {
+    {
+      "catppuccin/nvim",
+      as = "catppuccin",
+      config = function()
+        require("catppuccin").setup {
+          flavour = "mocha", -- latte, frappe, macchiato, mocha
+          background = { -- :h background
+            light = "latte",
+            dark = "mocha",
+          },
+          transparent_background = false,
+          show_end_of_buffer = false, -- show the '~' characters after the end of buffers
+          term_colors = false,
+          dim_inactive = {
+            enabled = false,
+            shade = "dark",
+            percentage = 0.15,
+          },
+          no_italic = false, -- Force no italic
+          no_bold = false, -- Force no bold
+          styles = {
+            comments = { "italic" },
+            conditionals = { "italic" },
+            loops = {},
+            functions = {},
+            keywords = {},
+            strings = {},
+            variables = {},
+            numbers = {},
+            booleans = {},
+            properties = {},
+            types = {},
+            operators = {},
+          },
+          color_overrides = {},
+          custom_highlights = {},
+          integrations = {
+            cmp = true,
+            gitsigns = true,
+            nvimtree = true,
+            telescope = true,
+            notify = false,
+            mini = false,
+          },
+        }
+      end,
+    },
+  },
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
